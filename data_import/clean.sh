023156eb65655422c3c55618a6206e2011563560
root <root@static.204.115.47.78.clients.your-server.de>
2013-12-14 14:50:21 +0100

    chenged directories in cleaning script

--- data_import/clean.sh
+++ data_import/clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /root/virtual_balloon
-sqlite3 ../data/weather_data.db "delete from wind where time < date('now')"
-sqlite3 ../data/weather_data.db "vacuum"
+cd /root/vb
+sqlite3 data/weather_data.db "delete from wind where time < date('now')"
+sqlite3 data/weather_data.db "vacuum"
 date > cronlog.txt
 
-rm grib_files/*.grib
+rm data/grib_files/*.grib
