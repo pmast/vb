29920df400d1b2c38be92773f08578bcd57afe19
root <root@static.204.115.47.78.clients.your-server.de>
2013-12-28 16:23:56 +0100

    some changes to simplify test

--- simplify_test.js
+++ simplify_test.js
@@ -1,12 +1,17 @@
-var s = require('simplify-js');
+var s = require('simplify.js');
 
 var mongoose = require('mongoose');
 var BalloonHistory = require('./models/balloon_history_model');
 
-history = BalloonHistory.find({balloonID: ObjectId("52ad7db2c2132bc239000002"});
-h2 = history.map(function(el){
-	return el.location;
+history = BalloonHistory.find({balloonID: "52ad7db2c2132bc239000002"},function(err, items){
+	if (err) throw err;
+	console.log(items);
+	console.log("peter");
 });
-console.log(h2.length)
-h2_simplified = s(l);
-console.log(h2_simplified.length);
+console.log('haus');
+//h2 = history.map(function(el){
+//	return el.location;
+//});
+//console.log(h2.length)
+//h2_simplified = s(l);
+//console.log(h2_simplified.length);
