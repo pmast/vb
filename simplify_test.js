b60718cc5a92d382bc259319b4b36dd4bbf5f993
pat <patrick.mast@gmx.net>
2013-12-28 16:33:57 +0100

    updated simplify test

--- simplify_test.js
+++ simplify_test.js
@@ -1,17 +1,16 @@
 var s = require('simplify.js');
 
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vb');
 var BalloonHistory = require('./models/balloon_history_model');
 
-history = BalloonHistory.find({balloonID: "52ad7db2c2132bc239000002"},function(err, items){
+history = BalloonHistory.find({balloonID: "52b7ef49218388be10000002"}, 	'location', function(err, items){
 	if (err) throw err;
-	console.log(items);
-	console.log("peter");
+	h = items.map(function(el){
+		return el.location;
+	});
+	console.log(h.length);
+	h_s = s(h);
+	console.log(h_s.length);
+	mongoose.disconnect();
 });
-console.log('haus');
-//h2 = history.map(function(el){
-//	return el.location;
-//});
-//console.log(h2.length)
-//h2_simplified = s(l);
-//console.log(h2_simplified.length);
