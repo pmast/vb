f2d9e81456e90cd6a5e07cd9e92cc316ae78ac11
pat <patrick.mast@gmx.net>
2013-12-28 22:39:34 +0100

    created cleaning script

---
+++ clean.js
@@ -0,0 +1,57 @@
+var async = require('async');
+
+var mongoose = require('mongoose');
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+mongoose.connection.on("error", function(errorObject){
+  console.log(errorObject);
+});
+
+mongoose.connect('mongodb://localhost/vb');
+var Balloon = require('./models/balloon_model');
+var BalloonHistory = require('./models/balloon_history_model');
+
+// find one with item for that balloon and that timestamp
+// keep the item
+// remove all items with tha same balloondid, longitude, latitude and timestamp but different ids
+function rm(balloon, date, cb3){
+	BalloonHistory.findOne({balloonID: balloon._id, 'location.timestamp': date}, function(err, keep){
+			console.log('keep: ' + keep);
+    		BalloonHistory.find({
+    			balloonID: balloon._id,
+    			'location.timestamp': date
+    		}, function(err, results){
+    			if (err) throw err;
+    			console.log('length: ' + results.length);
+    			console.log('removed: ' + balloon.id + ", " + date);
+    			cb3();
+    		});
+    	});
+}
+
+// for every balloon
+// get all distinct timestamps
+// go and remove all dublicates for that balloon id and the timestamps
+function clean(balloon, cb){
+	BalloonHistory.find({balloonID: balloon._id}).distinct('location.timestamp', function(error, ids) {
+    	console.log(ids[0]);
+    	async.each(ids, function(el, cb2){
+    		rm(balloon, el, cb2);
+    	}, function(err){
+    		if (err) throw err;
+    		console.log('hans');
+    		cb()
+    	})
+    	
+	});
+}
+
+Balloon.find(function(err, balloons){
+	if (err) throw err;
+	// console.log(balloons);
+	async.each(balloons, clean, function(err){
+		if (err) throw err;
+		mongoose.disconnect();
+	});
+});
